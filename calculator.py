45ed52509cc8197d2c0497d1a3ad8ba3e1a7f282
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 15:36:03 -0800

    Power function working

---
+++ calculator.py
@@ -0,0 +1,18 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+# Your code goes here
+while True:
+    input = raw_input("Input expression to calculate: ")
+    tokens = input.split(" ")
+    if tokens[0] == "q":
+        break
+    else:
+        if tokens[0] == "pow":
+            print power(int(tokens[1]), int(tokens[2]))
