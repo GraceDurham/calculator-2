c5714f9d2cb3457d51547dd7e48c7e250da51ebf
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-05 12:57:00 -0800

    Working version with Try ExceptValue

--- calculator.py
+++ calculator.py
@@ -9,30 +9,41 @@ from arithmetic import *
 
 # Your code goes here
 while True:
+    
     user_input = raw_input("Input expression to calculate: ")
     tokens = user_input.split()
     print tokens
+     
+
+    try:
+        num_1=int(tokens[1])
+        num_2=int(tokens[2])
     
-    num_1=int(tokens[1])
-    num_2=int(tokens[2])
-    if tokens[0] == "q":
-        break
+
     
-    elif tokens[0] == "pow" and len(tokens) == 3:
-        print power(num_1,num_2)
-    elif tokens[0] == "+" and len(tokens) == 3:
-        print add(num_1, num_2)
-    elif tokens[0] == "-" and len(tokens) == 3 and type(tokens[1]) == int and type(tokens[2] == int):
-        print subtract(int(tokens[1]), int(tokens[2]))
-    elif tokens[0] == "*" and len(tokens) == 3 and type(tokens[1]) == int and type(tokens[2] == int):
-        print multiply(int(tokens[1]), int(tokens[2]))
-    elif tokens[0] == "/" and len(tokens) == 3 and type(tokens[1]) == int and type(tokens[2] == int):
-        print divide(int(tokens[1]), int(tokens[2]))
-    elif tokens[0] == "square" and len(tokens) == 2 and type(tokens[1]) == int:
-        print square(int(tokens[1]))
-    elif tokens[0] == "cube" and len(tokens) == 2 and type(tokens[1]) ==int:
-        print cube(int(tokens[1]))
-    elif tokens[0] == "mod" and len(tokens) == 3 and type(tokens[1]) == int and type(tokens[2] == int):
-        print mod(int(tokens[1]), int(tokens[2]))
-    else:
-        print "Invalid input please choose one of the following operators +, -, *, /, square, cube, pow, mod or check the number arguments. Enter q to quit."
+        if tokens[0] == "q":
+            break
+        
+        elif tokens[0] == "pow" and len(tokens) == 3:
+            print power(num_1,num_2)
+        elif tokens[0] == "+" and len(tokens) == 3:
+            print add(num_1, num_2)
+        elif tokens[0] == "-" and len(tokens) == 3:
+            print subtract(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "*" and len(tokens) == 3:
+            print multiply(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "/" and len(tokens) == 3:
+            print divide(int(tokens[1]), int(tokens[2]))
+        elif tokens[0] == "square" and len(tokens) == 2:
+            print square(int(tokens[1]))
+        elif tokens[0] == "cube" and len(tokens) == 2:
+            print cube(int(tokens[1]))
+        elif tokens[0] == "mod" and len(tokens) == 3:
+            print mod(int(tokens[1]), int(tokens[2]))
+        else:
+            print "Invalid input please choose one of the following operators +, -, *, /, square, cube, pow, mod or check the number arguments. Enter q to quit."
+
+
+
+    except ValueError:
+        print "oops! this is not a number please try one more time"
