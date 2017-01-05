295e2f8d23b6a482c0d2094c4b37d895a069e895
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 16:24:59 -0800

    Updated conditionals to elif form nested if statements easier to read

--- calculator.py
+++ calculator.py
@@ -10,25 +10,26 @@ from arithmetic import *
 # Your code goes here
 while True:
     input = raw_input("Input expression to calculate: ")
-    tokens = input.split(" ")
+    tokens = input.split()
+    
     if tokens[0] == "q":
         break
+    
+    elif tokens[0] == "pow" and len(tokens) == 3:
+        print power(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == "+" and len(tokens) == 3:
+        print add(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == "-" and len(tokens) == 3:
+        print subtract(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == "*" and len(tokens) == 3:
+        print multiply(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == "/" and len(tokens) == 3:
+        print divide(int(tokens[1]), int(tokens[2]))
+    elif tokens[0] == "square" and len(tokens) == 2:
+        print square(int(tokens[1]))
+    elif tokens[0] == "cube" and len(tokens) == 2:
+        print cube(int(tokens[1]))
+    elif tokens[0] == "mod" and len(tokens) == 3:
+        print mod(int(tokens[1]), int(tokens[2]))
     else:
-        if tokens[0] == "pow" and len(tokens) == 3:
-            print power(int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "+" and len(tokens) == 3:
-            print add(int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "-" and len(tokens) == 3:
-            print subtract(int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "*" and len(tokens) == 3:
-            print multiply(int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "/" and len(tokens) == 3:
-            print divide(int(tokens[1]), int(tokens[2]))
-        if tokens[0] == "square" and len(tokens) == 2:
-            print square(int(tokens[1]))
-        if tokens[0] == "cube" and len(tokens) == 2:
-            print cube(int(tokens[1]))
-        if tokens[0] == "mod" and len(tokens) == 3:
-            print mod(int(tokens[1]), int(tokens[2]))
-        else:
-            print "Invalid input please choose one of the following operators +, -, *, /, square, cube, pow, mod or check the number arguments. Enter q to quit."
+        print "Invalid input please choose one of the following operators +, -, *, /, square, cube, pow, mod or check the number arguments. Enter q to quit."
